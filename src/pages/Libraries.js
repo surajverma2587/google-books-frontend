331cdd6194cf06bc08ebcdedd2f9ecf32c51cb41
surajverma2587 <surajverma2587@gmail.com>
2022-02-24 21:30:20 +0000

    wip

--- src/pages/Libraries.js
+++ src/pages/Libraries.js
@@ -1,3 +1,33 @@
+import { gql, useQuery } from "@apollo/client";
+import Container from "react-bootstrap/Container";
+import Stack from "react-bootstrap/Stack";
+import Spinner from "react-bootstrap/Spinner";
+
+const LIBRARIES = gql`
+  query Libraries {
+    libraries {
+      id
+      name
+      bookCount
+    }
+  }
+`;
+
 export const Libraries = () => {
-  return <h1>Libraries</h1>;
+  const libraries = useQuery(LIBRARIES);
+
+  return (
+    <Container fluid>
+      <Stack gap={5} className="text-center mt-5">
+        <h1>My Libraries</h1>
+        <hr />
+        {libraries.loading && (
+          <Spinner animation="border" role="status">
+            <span className="visually-hidden">Loading...</span>
+          </Spinner>
+        )}
+        {libraries?.data?.libraries && <div>TODO</div>}
+      </Stack>
+    </Container>
+  );
 };
