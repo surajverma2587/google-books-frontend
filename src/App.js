d9a7f3f67e9fe293ffc29511a2e2bca50e3ff124
surajverma2587 <surajverma2587@gmail.com>
2022-02-26 19:23:01 +0000

    wip

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
 import { BrowserRouter } from "react-router-dom";
-import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
 
 import { AppRoutes } from "./AppRoutes";
 import { NavigationBar } from "./components/NavigationBar";
 
-const client = new ApolloClient({
+const httpLink = createHttpLink({
   uri: "http://localhost:4000",
+});
+
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  // const token = localStorage.getItem('token');
+  const token =
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7ImVtYWlsIjoiYm9iLnNtaXRoQGVtYWlsLmNvbSIsIm5hbWUiOiJCb2IgU21pdGgiLCJpZCI6IjYyMWEyNTYxNzdmYzhlODdiZmJlN2FhOCJ9LCJpYXQiOjE2NDU4ODE3NDIsImV4cCI6MTY0NTg4ODk0Mn0.o19Q-mwc9afEJ5m3f8bGTSuyXlr-jdwWPjQ_5gEmO5M";
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
 
